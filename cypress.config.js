71cfecd5d3b7a266e9e1b5e7568e766eafc2a648
Anastasiapas <pasichnyk.nas23@gmail.com>
2024-09-05 14:17:22 -0700

    Cypress set up

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,16 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
     // Configure your E2E tests here
-    specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}"
+    specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}",
+    viewportWidth: 1180,
+    viewportHeight: 730,
+    defaultCommandTimeout: 2000,
+    pageLoadTimeout: 50000,
+    retries: {
+      runMode: 2,
+      openMode: 0,
+    },
   },
-})
+});
